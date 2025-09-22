d491e45fcce0868304f58421b22385d6253c197d
gabedown <gabedown4543@gmail.com>
2025-09-22 15:55:12 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,55 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+     toggleGrid();
 
     // TODO 2 - Create Platforms
+    createPlatform(50, 200, 100, 20, "blue");
+    createPlatform(100, 200, 300, 20, "blue");
+    createPlatform(400, 200, 20, 100, "blue");
+    createPlatform(400, 300, 200, 20, "blue");
+    createPlatform(600, 200, 20, 120, "blue");
+    createPlatform(600, 200, 600, 20, "blue");
+    createPlatform(1380, 0, 20, 400, "blue");
+    createPlatform(1000, 400, 600, 20, "blue");
+    createPlatform(700, 400, 600, 20, "blue");
+    createPlatform(700, 400, 20, 100, "blue");
+    createPlatform(500, 500, 220, 20, "blue");
+    createPlatform(400, 500, 200, 20, "blue");
+    createPlatform(50, 200, 20, 200, "blue");
+    createPlatform(300, 500, 100, 20, "blue");
+    createPlatform(300, 400, 20, 100, "blue");
+    createPlatform(200, 400, 100, 20, "blue");
+    createPlatform(200, 400, 10, 10000, "blue");
+  createPlatform(9, 499, 200, 20, "blue", 300, 500, 1)
+  createPlatform(100, 220, -300, -20, "blue")
+  createPlatform(70, 750, -20, -350, "blue")
+  
 
 
 
+
+  
+    
 
     // TODO 3 - Create Collectables
+  createCollectable("jem", 10, 500, 0.5, 0.7);
+  createCollectable("jem", 500, 200, 0.5, 0.7);
+  createCollectable("jem", 500, 460, 0, 1);
 
 
 
-    
+
+
+
+
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("top", 400, 900);
+    createCannon("top", 300, 800);
+    createCannon("right", 200, 3000);
+    createCannon("bottom", 300, 2000);
+    createCannon("right", 300, 5000);
+    createCannon("right", 400, 2000);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
@@ -52,4 +83,3 @@ $(function () {
 
   registerSetup(setup);
 });
-createPlatform(200, 200) 
--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -98,4 +98,5 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  jem: {image: "images/collectables/heartGem.png"}
 };
