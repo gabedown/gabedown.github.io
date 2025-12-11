9bb151b301501d0164cd4a8c0c19647542dafefe
gabedown <gabedown4543@gmail.com>
2025-12-11 16:20:27 +0000

    jjjjjj

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -51,22 +51,12 @@ var init = function (window) {
     function update() {
       // TODO 4 : Update the position of each circle using physikz.updatePosition()
       // TODO 5 : Call game.checkCirclePosition() on your circles
-      physikz.updatePosition(circles[0]);
-      physikz.updatePosition(circles[1]);
-      physikz.updatePosition(circles[2]);
-      physikz.updatePosition(circles[3]);
-      physikz.updatePosition(circles[4]);
-
-      game.checkCirclePosition(circles[0]);
-      game.checkCirclePosition(circles[1]);
-      game.checkCirclePosition(circles[2]);
-      game.checkCirclePosition(circles[3]);
-      game.checkCirclePosition(circles[4]);
-
       // TODO 8 / TODO 9 : Iterate over the array
-    }
-    for (var circlesi = 0; i < circles.length; i++) {
-    pdateCirclePosition, physikz.updatePosition
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+        console.log("hello funyuns");
+      }
     }
 
     /* 
