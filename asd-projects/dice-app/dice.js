93d79be5de6f586bc40d95f484daffb51b98b5c9
gabedown <gabedown4543@gmail.com>
2026-02-26 17:35:37 +0000

    gh

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,12 +1,91 @@
 $(document).ready(function () {
   // Your code goes here
-$("<div>")
-.css("height", 75)
-.css("width", 75)
-.css("width," 75)
-.css("background-color", "blue")
-.css("postition", "absolute")
-.css("top", 50)
- .css("left", 50)
-.appendTo("#die");
+  function makeDot(top, left, elementId) {
+    $("<div>")
+      .css("height", 75)
+      .css("width", 75)
+      .css("background-color", "black")
+      .css("position", "absolute")
+      .css("top", top)
+      .css("left", left)
+      .css("border-radius", "50%")
+      .appendTo(elementId)
+      .appendTo("#die");
+  }
+  function rollDie(dieId) {
+    $(dieId).empty();
+    console.log("clicked");
+    var randomNum = Math.ceil(Math.random() * 6);
+    console.log(randomNum);
+    if (randomNum === 1) {
+      makeDot(42.5, 42.5, dieId);
+      // middle middle
+    } else if (randomNum === 2) {
+      makeDot(17.5, 17.5, dieId);
+      // top left
+
+      makeDot(67.5, 67.5, dieId);
+      // bottom right
+    } else if (randomNum === 3) {
+      makeDot(17.5, 17.5, dieId);
+      // top left
+
+      makeDot(67.5, 67.5, dieId);
+      // bottom right
+
+      makeDot(42.5, 42.5, dieId);
+      // middle middle
+    } else if (randomNum === 4) {
+      makeDot(67.5, 67.5, dieId);
+      // bottom right
+
+      makeDot(17.5, 17.5, dieId);
+      // top left
+
+      makeDot(17.5, 67.5, dieId);
+      // top right
+
+      makeDot(67.5, 17.5, dieId);
+      // bottom left
+    } else if (randomNum === 5) {
+      makeDot(42.5, 42.5, dieId);
+      // middle middle
+
+      makeDot(67.5, 67.5, dieId);
+      // bottom right
+
+      makeDot(17.5, 17.5, dieId);
+      // top left
+
+      makeDot(17.5, 67.5, dieId);
+      // top right
+
+      makeDot(67.5, 17.5, dieId);
+      // bottom left
+    } else if (randomNum === 6) {
+      makeDot(67.5, 67.5, dieId);
+      // bottom right
+
+      makeDot(17.5, 17.5, dieId);
+      // top left
+
+      makeDot(17.5, 67.5, dieId);
+      // top right
+
+      makeDot(67.5, 17.5, dieId);
+      // bottom left
+
+      makeDot(42.5, 67.5, dieId); // middle right
+
+      makeDot(42.5, 17.5, dieId);
+    }
+    // middle left
+  }
+  //makeDot(212.5, 212.5, "#die");
+  //makeDot(87.5, 87.5, "#die");
+  //makeDot(337.5, 377.5, "#die");
+  function handleClick() {
+    rollDie("#die");
+  }
+  $("#die").on("click", handleClick);
 });
