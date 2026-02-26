4c5fb77d0e5d71ce1f78848061c3bf1ee3c5b836
gabedown <gabedown4543@gmail.com>
2026-02-26 16:25:04 +0000

    boo

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
   // Your code goes here
-
+$("<div>")
+.css("height", 75)
+.css("width", 75)
+.css("width," 75)
+.css("background-color", "blue")
+.css("postition", "absolute")
+.css("top", 50)
+ .css("left", 50)
+.appendTo("#die");
 });
