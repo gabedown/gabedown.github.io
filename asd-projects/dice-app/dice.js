fb4f41d89f391a76ce0e9689f0d4931e74ea3b8a
gabedown <gabedown4543@gmail.com>
2026-02-27 16:33:58 +0000

    jd

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -17,71 +17,64 @@ $(document).ready(function () {
     console.log("clicked");
     var randomNum = Math.ceil(Math.random() * 6);
     console.log(randomNum);
-    if (randomNum === 1) {
-      makeDot(42.5, 42.5, dieId);
-      // middle middle
+if (randomNum === 1) {
+      makeDot(212.5, 212.5, dieId); // middle middle
     } else if (randomNum === 2) {
-      makeDot(17.5, 17.5, dieId);
-      // top left
-
-      makeDot(67.5, 67.5, dieId);
-      // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(337.5, 337.5, dieId); // bottom right
     } else if (randomNum === 3) {
-      makeDot(17.5, 17.5, dieId);
-      // top left
-
-      makeDot(67.5, 67.5, dieId);
-      // bottom right
-
-      makeDot(42.5, 42.5, dieId);
-      // middle middle
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(212.5, 212.5, dieId); // middle middle
     } else if (randomNum === 4) {
-      makeDot(67.5, 67.5, dieId);
-      // bottom right
-
-      makeDot(17.5, 17.5, dieId);
-      // top left
-
-      makeDot(17.5, 67.5, dieId);
-      // top right
-
-      makeDot(67.5, 17.5, dieId);
-      // bottom left
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // bottom left
+      makeDot(337.5, 87.5, dieId); // top right
     } else if (randomNum === 5) {
-      makeDot(42.5, 42.5, dieId);
-      // middle middle
-
-      makeDot(67.5, 67.5, dieId);
-      // bottom right
-
-      makeDot(17.5, 17.5, dieId);
-      // top left
-
-      makeDot(17.5, 67.5, dieId);
-      // top right
-
-      makeDot(67.5, 17.5, dieId);
-      // bottom left
+      makeDot(212.5, 212.5, dieId); // middle middle
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // top right
+      makeDot(337.5, 87.5, dieId); // bottom left
     } else if (randomNum === 6) {
-      makeDot(67.5, 67.5, dieId);
-      // bottom right
-
-      makeDot(17.5, 17.5, dieId);
-      // top left
-
-      makeDot(17.5, 67.5, dieId);
-      // top right
-
-      makeDot(67.5, 17.5, dieId);
-      // bottom left
-
-      makeDot(42.5, 67.5, dieId); // middle right
-
-      makeDot(42.5, 17.5, dieId);
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // top right
+      makeDot(337.5, 87.5, dieId); // bottom left
+      makeDot(212.55, 87.5, dieId); // middle left
+      makeDot(212.5, 337.5, dieId); // middle right
+    }if (randomNum === 1) {
+      makeDot(212.5, 212.5, dieId); // middle middle
+    } else if (randomNum === 2) {
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(337.5, 337.5, dieId); // bottom right
+    } else if (randomNum === 3) {
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(212.5, 212.5, dieId); // middle middle
+    } else if (randomNum === 4) {
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // bottom left
+      makeDot(337.5, 87.5, dieId); // top right
+    } else if (randomNum === 5) {
+      makeDot(212.5, 212.5, dieId); // middle middle
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // top right
+      makeDot(337.5, 87.5, dieId); // bottom left
+    } else if (randomNum === 6) {
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // top right
+      makeDot(337.5, 87.5, dieId); // bottom left
+      makeDot(212.55, 87.5, dieId); // middle left
+      makeDot(212.5, 337.5, dieId); // middle right
     }
     // middle left
   }
-  //makeDot(212.5, 212.5, "#die");
+  //makeDot(212.5, 212.5, "#die");                     
   //makeDot(87.5, 87.5, "#die");
   //makeDot(337.5, 377.5, "#die");
   function handleClick() {
